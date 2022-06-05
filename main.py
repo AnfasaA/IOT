1677e7b4287e0ef375572d67c393bdabacfb32d0
Clement Prolifel Priyatama <clementprolifel@gmail.com>
2022-06-05 17:34:44 +0700

    check

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import io
 import picamera
 import cv2
 import numpy
+import time
 
 #Create a memory stream so photos doesn't need to be saved in a file
 stream = io.BytesIO()
@@ -9,7 +10,8 @@ stream = io.BytesIO()
 #Get the picture (low resolution, so it should be quite fast)
 #Here you can also specify other parameters (e.g.:rotate the image)
 with picamera.PiCamera() as camera:
-    #camera.resolution = (320, 240)
+    time.sleep(2)
+    camera.resolution = (320, 240)
     camera.capture(stream, format='jpeg')
 
 #Convert the picture into a numpy array
