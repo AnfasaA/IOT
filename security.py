0ec507aa9a8f9fe0fa51c0570ef4fe063c682e25
AnfasaA <anfasa48@gmail.com>
2022-06-05 16:17:26 +0100

    update code

--- security.py
+++ security.py
@@ -16,6 +16,7 @@ while True:
         with picamera.PiCamera() as camera:
             camera.resolution = (320, 240)
             camera.framerate = 24
+            time.sleep(2)
             image = np.empty((240 * 320 * 3,), dtype=np.uint8)
             camera.capture(image, 'bgr')
             image = image.reshape((240, 320, 3))
